770eb29e689c7e8a6f3c28b404459b3ec78d9aa3
Candice_SXC <candicesxc17@gmail.com>
2026-02-01 05:31:32 -0500

    Update Credentials instantiation in app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-# Your current file content with the necessary change made
-
-# import statements
-from google.oauth2.credentials import Credentials
-
-# ...
-
-# line 92
-credentials = Credentials.from_authorized_user(info) # Removed the scopes parameter
-
-# ...
+def get_calendar_service():
+    # 
+    # ... other code ...
+    
+    creds_data = {}  # Ensure creds_data is defined or fetched correctly
+    creds = Credentials(token=creds_data.get('token'), refresh_token=creds_data.get('refresh_token'), token_uri=creds_data.get('token_uri', 'https://oauth2.googleapis.com/token'), client_id=creds_data.get('client_id'), client_secret=creds_data.get('client_secret'))
+    
+    # ... other code ...
